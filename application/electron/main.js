b3db57281fc72a0961efd3d8c60f2b76ac55dc2c
Padmanabhan <padmanabhan.rajendrakumar@gmail.com>
2026-01-24 21:32:37 +0530

    feat: Update electron startup script to use cross-env for environment variable management

--- application/electron/main.js
+++ application/electron/main.js
@@ -132,7 +132,8 @@ function createWindow() {
   
   if (isDev) {
     mainWindow.loadURL('http://localhost:5173');
-    mainWindow.webContents.openDevTools();
+    // Comment out the line below to stop DevTools from opening automatically
+    // mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
   }
